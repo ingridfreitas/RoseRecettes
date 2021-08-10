c72ac8e22e08c2fb302f95b6d4d00593322b3ed9
ingrid <ingridheloisesantos@gmail.com>
2021-08-09 22:06:48 -0300

    Grupo Bia and me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BebidasComponent } from './bebidas/bebidas.component';
+import { DocesComponent } from './doces/doces.component';
+import { HomeComponent } from './home/home.component';
+import { SalgadosComponent } from './salgados/salgados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'doces', component: DocesComponent},
+  {path: 'salgados', component: SalgadosComponent},
+  {path: 'bebidas', component: BebidasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { DocesComponent } from './doces/doces.component';
+import { SalgadosComponent } from './salgados/salgados.component';
+import { BebidasComponent } from './bebidas/bebidas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DocesComponent,
+    SalgadosComponent,
+    BebidasComponent
   ],
   imports: [
     BrowserModule,
